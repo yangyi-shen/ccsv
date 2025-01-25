885cd6b17ef1ad962d328f5d77769f2d6bf3d858
Yang-Yi-Shen <mrshenyangyi@gmail.com>
2025-01-25 16:29:55 +0800

    test: add malloc null handling to list code

--- list.c
+++ list.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef struct
 {
@@ -19,6 +20,10 @@ List list_initialize(void)
 void list_append(List *list, char *value)
 {
     list->pointer = realloc(list->pointer, sizeof(char *) * (list->length + 1)); // reallocate enough space for current contents + 1 char
+    if (!list->pointer) {
+        throw_error("issue allocating memory");
+        exit(EXIT_FAILURE);
+    }
     list->pointer[list->length] = strdup(value);                                 // assign new value to newly created index
     list->length++;                                                              // update length to match current contents
 }

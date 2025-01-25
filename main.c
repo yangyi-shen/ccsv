735176cf45e7c4f29753bafa043f9e515550dd31
Yang-Yi-Shen <mrshenyangyi@gmail.com>
2025-01-25 18:53:12 +0800

    refactor: move some functions to utils.c

--- main.c
+++ main.c
@@ -13,32 +13,6 @@ typedef struct
     // add column and main data handling later
 } DataFrame;
 
-long get_file_size(FILE *file)
-{
-    fseek(file, 0L, SEEK_END);
-    long fileSize = ftell(file);
-    rewind(file);
-
-    return fileSize;
-}
-
-char *get_file_text(FILE *file)
-{
-    const long fileSize = get_file_size(file);
-    char *fileText = malloc(fileSize + 1); // add 1 to fileSize to reserve space for the null terminator
-    if (!fileText)
-    {
-        throw_error("issue allocating memory");
-        fclose(file);
-        exit(EXIT_FAILURE);
-    }
-
-    fread(fileText, 1, fileSize, file); // read file in fileSize chunks of 1 byte
-    fileText[fileSize] = '\0';          // null-terminate
-
-    return fileText;
-}
-
 DataFrame parse_file_text(char *text)
 {
     char *headersText = strtok(text, "\n");
@@ -81,7 +55,7 @@ int main(void)
     char *fileText = get_file_text(ratingsFile);
 
     DataFrame parsedFileText = parse_file_text(fileText);
-    
+
     dataframe_print(parsedFileText);
 
     fclose(ratingsFile);
